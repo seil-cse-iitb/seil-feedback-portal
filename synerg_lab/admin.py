3396c60a388fa1df47d25d8805545f98a5cf5a3b
sapan <sapantanted99@gmail.com>
2017-11-22 13:05:41 +0530

    Database ready and migrations is done

--- synerg_lab/admin.py
+++ synerg_lab/admin.py
@@ -3,17 +3,29 @@ from django.contrib import admin
 from synerg_lab.models import Question
 
 
-# Register your models here.
-
-
 class QuestionAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_on'
-    fields = ['id','text', 'answer_type']
-    list_display = ('id', 'text','answer_type')
-    list_display_links = ['text','answer_type']
+    fields = ['id','question_text', 'answer_type']
+    list_display = ('id', 'question_text','answer_type')
+    list_display_links = ['question_text','answer_type']
     list_filter = ['id']
     actions_on_bottom = True
     readonly_fields = ('id',)
 
 
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question,QuestionAdmin)
+
+#
+# # Register your models here.
+#
+# class QuestionDemoAdmin(admin.ModelAdmin):
+#     date_hierarchy = 'created_on'
+#     fields = ['id','text', 'answer_type']
+#     list_display = ('id', 'text','answer_type')
+#     list_display_links = ['text','answer_type']
+#     list_filter = ['id']
+#     actions_on_bottom = True
+#     readonly_fields = ('id',)
+#
+#
+# admin.site.register(QuestionDemo, QuestionDemoAdmin)
--- synerg_lab/models.py
+++ synerg_lab/models.py
@@ -1,17 +1,105 @@
 from django.db import models
 
 
-# Create your models here.
+class CommonModel(models.Model):
+    id = models.AutoField
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
 
-class Question(models.Model):
-    TEXT = 'text'
+    class Meta:
+        abstract = True
+
+
+class Room(CommonModel):
+    name = models.CharField(max_length=100,
+                            help_text="What is the room generally called? ex:- seil lab,circular hall etc")
+    short_name = models.CharField(max_length=50, help_text="Short name of the room. ex:- SIC 201,SIA 310 etc")
+
+    def __str__(self):
+        return "%s the room aka %s" % (self.name, self.short_name)
+
+
+class Region(CommonModel):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, help_text="This will be displayed to users while giving feedback.")
+    description = models.CharField(max_length=50, blank=True, help_text="Describe the region if required")
+
+    def __str__(self):
+        return "%s the region in %s" % (self.name, self.room)
+
+
+class Sensor(CommonModel):
+    HUMIDITY = 'H'
+    TEMPERATURE = 'T'
+    HUMIDITY_TEMPERATURE = 'H_T'
+    sensor_type_choices = ((None, "Choose sensor type!"),
+                           (HUMIDITY, "Humidity"),
+                           (TEMPERATURE, "Temperature"),
+                           (HUMIDITY_TEMPERATURE, "Humidity and Temperature"),
+                           )
+    sensor_type = models.CharField(max_length=50, choices=sensor_type_choices)
+    region = models.ManyToManyField(Region)
+
+    def __str__(self):
+        return "%s the sensor in %s" % (self.id, self.region)
+
+
+class User(CommonModel):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, help_text="Full name of user")
+    username = models.CharField(max_length=50, help_text="username for login", primary_key=True)
+    password = models.CharField(max_length=50, help_text="password for login")
+    email = models.EmailField(max_length=100, help_text="Email for contacting the user")
+    height = models.FloatField(help_text="Meta data for future study(if available)", blank=True, null=True)
+    weight = models.FloatField(help_text="Meta data for future study(if available)", blank=True, null=True)
+
+    def __str__(self):
+        return "%s the user with username %s and email %s at room %s" % (
+            self.name, self.username, self.email, self.room)
+
+
+class Question(CommonModel):
+    region = models.ManyToManyField(Region)
+    question_text = models.CharField(max_length=500, help_text="This will be shown as question for feedback")
+    SINGLE = 'S'
+    MULTIPLE = 'M'
+    answer_type_choices = ((None, "Choose answer type!"),
+                           (SINGLE, "Single Choice Answer"),
+                           (MULTIPLE, "Multiple Choice Answer"),
+                           )
+    answer_type = models.CharField(max_length=50, choices=answer_type_choices,
+                                   help_text="Single Choice Answer will render radio button for answers while Multiple"
+                                             " Choice Answer will render check box for answers")
+
+    def __str__(self):
+        return "%s the question(%s) with question text %s" % (self.id, self.answer_type, self.question_text)
+
+
+class Answer(CommonModel):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200, help_text="This will be shown as a answer of related question")
+    user = models.ManyToManyField(User, through='UserAnswer')
+
+    def __str__(self):
+        return "%s the answer of %s" % (self.answer_text, self.question)
+
+
+class UserAnswer(CommonModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    answer_time = models.DateTimeField()
+
+    def __str__(self):
+        return "user %s at time %s gives answer %s" % (self.user, self.answer_time, self.answer)
+
+
+# Create your models here.
+class QuestionDemo(models.Model):
     SINGLE_CHOICE = 'single'
     MULTI_CHOICE = 'multi'
     answer_type_choices = (
         (None, "Choose a type for answers!"),
         (
             "Answer Type", [
-                (TEXT, 'Text'),
                 (SINGLE_CHOICE, 'Single choice'),
                 (MULTI_CHOICE, 'Multi choice'),
             ]
@@ -19,6 +107,6 @@ class Question(models.Model):
     )
     # Fields
     id = models.AutoField
-    text = models.CharField(max_length=500,help_text="This field is the question that will be shown")
+    text = models.CharField(max_length=500, help_text="This field is the question that will be shown")
     answer_type = models.CharField(max_length=10, choices=answer_type_choices)
-    created_on = models.DateTimeField(auto_now_add=True,editable=False)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
